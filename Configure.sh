c3d13a8b0a6aa46bff5e592769e53fa907e2f43e
aryandvn <aryudhawan2001@gmail.com>
2023-04-01 12:27:53 -0400

    Added Configurations

---
+++ Configure.sh
@@ -0,0 +1,38 @@
+#Volume
+Volume_Name=("db_vol" "Nexus" "Jenkins" "sonarqube_database_data" "sonarqube_bundled-plugins" "sonarqube_conf" "sonarqube_data" "sonarqube_database" "sonarqube_extensions")
+
+# Iterate through the array and print each name
+for name in ${Volume_Name[@]}
+do
+  #!/bin/bash
+
+    if docker volume ls | grep -q "$name"; then
+        echo "Volume $name already exists."
+    else
+        echo "Volume $name does not exist. Creating..."
+        docker volume create "$name"
+    fi
+done
+
+#Volume
+Image_Name=("aryandvn24/aryan-nexus" "aryandvn24/jenkinsmod" "sonarqube" "aryandvn24/aryan-pdb" "mysql:5.7" "phpmyadmin/phpmyadmin" "tomcat")
+
+# Iterate through the array and print each name
+for name in ${Image_Name[@]}
+do
+  #!/bin/bash
+
+    if docker images | grep -q "$name"; then
+        echo "Docker Image $name already exists."
+    else
+        echo "Docker Image $name does not exist. Creating..."
+        docker pull "$name"
+    fi
+done
+
+#Start docker compose up
+docker compose up -d
+#To show all the pulled images
+docker images
+#To show all the pulled containers
+docker ps
