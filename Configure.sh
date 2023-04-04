ca1e07e39cfd2d29914fbef58f8c92d20f2d6828
aryandvn <aryudhawan2001@gmail.com>
2023-04-04 02:23:38 -0400

    Changed Configure.sh and Added StopService.sh

--- Configure.sh
+++ Configure.sh
@@ -8,8 +8,8 @@ do
 
     if docker volume ls | grep -q "$name"; then
         echo "Volume $name already exists."
-        docker volume rm "$name"
-        docker volume create "$name"
+ 
+        #docker volume create "$name"
     else
         echo "Volume $name does not exist. Creating..."
         docker volume create "$name"
@@ -26,16 +26,36 @@ do
 
     if docker images | grep -q "$name"; then
         echo "Docker Image $name already exists."
-        docker rmi "$name"
-        docker pull "$name"
+	echo "Pulling the latest Images: $name"
+        #docker rmi "$name"
+        #docker pull "$name"
     else
         echo "Docker Image $name does not exist. Creating..."
         docker pull "$name"
     fi
 done
 
-#Moving to root directory
-cd /
+#Cleaning the Docker System
+docker system prune -f
 
-#Cloning Project Repository
-git clone https://github.com/aryandvn/BackUp.git 
+#Moving to root directory
+cd /home/weblogic/
+
+#Cloning Git Repository
+git clone https://github.com/aryandvn/BackUp.git
+
+#Moving to the backup Directory
+cd BackUp/
+
+#Running all the services.
+docker compose up -d
+
+#Showing all the images
+docker images
+
+#Showing all the Services up and running
+docker ps
+
+
+
+
---
+++ StopService.sh
@@ -0,0 +1,6 @@
+#This is a bash file that stops all the services
+
+cd /home/weblogic/BackUp/
+
+#To stop all the services
+docker compose down
