91d652b1e587d7e439d94c26fc431c2cd1ebf584
aryandvn <54626547+aryandvn@users.noreply.github.com>
2023-04-27 16:47:36 +0530

    Update StopService.sh

--- StopService.sh
+++ StopService.sh
@@ -2,8 +2,22 @@
 
 cd /home/weblogic/BackUp/
 
+echo ""
+echo "##########################################"
+echo "        Stopping Docker Services          "
+echo "##########################################"
+echo ""
+
 #To stop all the services
 docker compose down
 
 #Removing Diectory
 rm -rf /home/weblogic/BackUp/
+
+echo ""
+echo "##########################################"
+echo "      Stopping Kubernetes Services        "
+echo "##########################################"
+echo ""
+
+minikube stop
